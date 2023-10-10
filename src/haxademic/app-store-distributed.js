1b236570b69d51c8325ff305c669bcee86d71926
Olivia Ruiz-Knott <livvy@hovercraftstudio.com>
2023-10-10 13:30:24 -0600

    add local storage and sender id capabilities

--- src/haxademic/app-store-distributed.js
+++ src/haxademic/app-store-distributed.js
@@ -2,26 +2,68 @@ import AppStore from "./app-store.js";
 import SolidSocket from "./solid-socket.js";
 
 class AppStoreDistributed extends AppStore {
+  static CONNECTED = "AppStoreDistributed_CONNECTED";
+  static DISCONNECTED = "AppStoreDistributed_DISCONNECTED";
+  static CUSTOM_JSON = "CUSTOM_JSON";
+
   constructor(socketServerUrl) {
     super();
-    // init websock connection
+
+    // track whether messages are from this instance
+    this.messageFromSelf = false;
+
+    // init websocket connection
     this.socketServerUrl = socketServerUrl;
     this.solidSocket = new SolidSocket(socketServerUrl);
-    this.solidSocket.setOpenCallback(e => this.onOpen(e));
-    this.solidSocket.setMessageCallback(e => this.onMessage(e));
+    this.solidSocket.setOpenCallback((e) => this.onOpen(e));
+    this.solidSocket.setCloseCallback((e) => this.onClose(e));
+    this.solidSocket.setMessageCallback((e) => this.onMessage(e));
+  }
+
+  generateUUID() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+      var d = new Date().getTime();
+      d += performance.now(); // use high-precision timer if available
+      var r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+    });
+  }
+
+  initSenderID() {
+    this.uuid = this.generateUUID();
+    return this.uuid;
+  }
+
+  senderId() {
+    return this.uuid;
+  }
+
+  senderIsSelf() {
+    return this.messageFromSelf;
   }
 
   onOpen() {
-    console.log("AppStoreDistributed connected to " + this.socketServerUrl);
+    console.log("AppStoreDistributed connected: " + this.socketServerUrl);
     this.set(AppStoreDistributed.CONNECTED, this.socketServerUrl);
   }
 
+  onClose() {
+    console.log("AppStoreDistributed disconnected: " + this.socketServerUrl);
+    this.set(AppStoreDistributed.DISCONNECTED, this.socketServerUrl);
+  }
+
   onMessage(event) {
-    let jsonData = JSON.parse(event.data);
-    if (jsonData["store"] && jsonData["type"]) {
-      this.set(jsonData["key"], jsonData["value"]);
+    let data = JSON.parse(event.data);
+
+    // note whether sender is self, so we can check before taking action on incoming data that was sent by us, with senderIsSelf()
+    this.messageFromSelf = data && data.sender && data.sender == this.uuid;
+
+    // set incoming data on AppStore
+    if (data["store"] && data["type"]) {
+      this.set(data["key"], data["value"]);
     } else {
-      this.set("CUSTOM_JSON", jsonData);
+      this.set(AppStoreDistributed.CUSTOM_JSON, data);
       // this.set('json', event.data); // just to see data in AppStoreDebug
     }
   }
@@ -39,17 +81,16 @@ class AppStoreDistributed extends AppStore {
         store: true,
         type: type,
       };
+      if (this.uuid) data.sender = this.uuid;
       this.solidSocket.sendMessage(JSON.stringify(data)); // local AppStore is updated when message is broadcast back to us
     } else {
       super.set(key, value);
     }
   }
 
-  broadcastJson(obj) {
+  broadcastCustomJson(obj) {
     this.solidSocket.sendMessage(JSON.stringify(obj));
   }
 }
-
-AppStoreDistributed.CONNECTED = "AppStoreDistributed_CONNECTED";
 
 export default AppStoreDistributed;
--- src/haxademic/app-store.js
+++ src/haxademic/app-store.js
@@ -1,4 +1,6 @@
 class AppStore {
+  static LOCALSTORAGE_KEY = "_store";
+
   constructor() {
     this.state = {};
     this.listeners = [];
@@ -36,21 +38,46 @@ class AppStore {
     // specific listener methods
     const objs = this.methods[key];
     if (objs) {
-      objs.forEach(el => {
+      objs.forEach((el) => {
         if (el[key]) el[key](value);
         else throw new Error("AppStore listener has no callback: " + key);
       });
     }
+    // save to local storage
+    if (this.persistData) this.saveToLocalStorage(key);
   }
 
   get(key) {
     return this.state[key];
   }
 
+  toString() {
+    return JSON.stringify(this.state, null, 2);
+  }
+
   log() {
     for (let key in _store.state) {
       console.log(key, _store.state[key]);
     }
+  }
+
+  initLocalStorage(keyTriggerExclusions = []) {
+    this.persistData = true;
+    this.keyTriggerExclusions = keyTriggerExclusions; // these keys will not trigger a save to localStorage
+    const savedState = window.localStorage.getItem(AppStore.LOCALSTORAGE_KEY);
+    if (savedState) {
+      this.state = JSON.parse(savedState);
+    }
+  }
+
+  saveToLocalStorage(key) {
+    if (this.keyTriggerExclusions.includes(key)) return;
+    let storeData = JSON.stringify(this.state);
+    window.localStorage.setItem(AppStore.LOCALSTORAGE_KEY, storeData);
+  }
+
+  clearLocalStorage() {
+    window.localStorage.removeItem(AppStore.LOCALSTORAGE_KEY);
   }
 }
 
